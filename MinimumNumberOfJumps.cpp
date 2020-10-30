9cdf488143934899e576635e74e77b605d2c6f70
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-10-30 19:39:52 +0530

    Update MinimumNumberOfJumps.cpp

--- MinimumNumberOfJumps.cpp
+++ MinimumNumberOfJumps.cpp
@@ -49,3 +49,37 @@ int main()
 	}
 	return 0;
 }
+
+//Greedy Approach 
+/*
+ Time complexity : O(N)
+ Space complexity : O(1)
+ */
+
+class Solution {
+public:
+    int jump(vector<int>& nums)
+    {
+        int n = nums.size();
+        int jumps =0;
+        int currMax =0 ;
+        int maxReach =0;
+        
+        for(int i=0; i < n-1; i++)
+        {
+             if( i + nums[i] > currMax )
+             {
+                 currMax = i + nums[i];
+             }
+            if( i == maxReach )
+            {
+                jumps++;
+                maxReach = currMax;
+            }
+                
+        }
+        
+        return jumps;
+        
+    }
+};
